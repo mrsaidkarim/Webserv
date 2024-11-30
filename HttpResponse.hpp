0f4c9255b73329f9693700593a06c1f1904bae00
zech-chi <machinelearning1.618@gmail.com>
2024-11-30 18:19:28 +0100

    .

---
+++ HttpResponse.hpp
@@ -0,0 +1,15 @@
+#ifndef HTTPRESPONSE_HPP
+#define HTTPRESPONSE_HPP
+
+#include "../const.hpp"
+#include "../Configuration/Server.hpp"
+#include "../Request/HttpRequest.hpp"
+
+class HttpResponse {
+    private:
+        HttpRequest
+
+    public:
+};
+
+#endif
