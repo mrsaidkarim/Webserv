45bbf183c97f3a2326f36401e613c26ab33388b0
zelabbas <zakariaelabbassi2002@gmail.com>
2024-12-01 12:33:31 +0100

    a small update in Makefile && main

--- main.cpp
+++ main.cpp
@@ -3,75 +3,89 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: skarim <skarim@student.42.fr>              +#+  +:+       +#+        */
+/*   By: zelabbas <zelabbas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 12:06:33 by skarim            #+#    #+#             */
-/*   Updated: 2024/11/30 14:14:26 by skarim           ###   ########.fr       */
+/*   Updated: 2024/12/01 12:27:59 by zelabbas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "const.hpp"
 #include "Configuration/Location.hpp"
 #include "Configuration/Server.hpp"
+#include "Request/HttpRequest.hpp"
 
-int main(void)
+int main(int ac, char **av)
 {
-    string route = "/";
-    vector<string> indexes = {"index.html", "home.html"};
-    bool auto_index = true;
-    string root = "/var/www/example";
-    map<string, bool> methods = {
-        {"GET", true},
-        {"POST", true},
-        {"DELETE", false}
-    };
-    pair<string, string> redirections = {"301", "www.youtube.com"};
+    // string route = "/";
+    // vector<string> indexes = {"index.html", "home.html"};
+    // bool auto_index = true;
+    // string root = "/var/www/example";
+    // map<string, bool> methods = {
+    //     {"GET", true},
+    //     {"POST", true},
+    //     {"DELETE", false}
+    // };
+    // pair<string, string> redirections = {"301", "www.youtube.com"};
 
-    // Create a Location object
-    Location loc1(route, indexes, auto_index, root, methods, redirections);
-    Location loc2(
-        "/home",
-        {"home.html"},
-        false,
-        "",
-        {{"GET", true},
-        {"POST", false},
-        {"DELETE", false}},
-        {}
-    );
-    Location loc3(
-        "/youtube",
-        {},
-        false,
-        "",
-        {{"GET", true},
-        {"POST", false},
-        {"DELETE", false}},
-        {"301", "www.youtube.com"}
-    );
-    // Location.print_lacation_info();
+    // // Create a Location object
+    // Location loc1(route, indexes, auto_index, root, methods, redirections);
+    // Location loc2(
+    //     "/home",
+    //     {"home.html"},
+    //     false,
+    //     "",
+    //     {{"GET", true},
+    //     {"POST", false},
+    //     {"DELETE", false}},
+    //     {}
+    // );
+    // Location loc3(
+    //     "/youtube",
+    //     {},
+    //     false,
+    //     "",
+    //     {{"GET", true},
+    //     {"POST", false},
+    //     {"DELETE", false}},
+    //     {"301", "www.youtube.com"}
+    // );
+    // // Location.print_lacation_info();
 
-    vector<int> ports = {8080, 443};
-    vector<string> server_names = {"zechi.com", "www.nigro.com"};
-    long long client_max_body_size = 10485760; // 10 MB
-    vector<Location> locations = {loc1, loc2, loc3};
-    string global_root = "/globalroot";
-    pair<string, string> redirection = {"301", "www.ingtagram.com"};
-    vector<string> indexes2 = {"indexserver.html", "homeserver.html"};
-    bool autoindex2 = true;
-    vector<string> error_pages = {"404.html", "500.html", "403.html"};
+    // vector<int> ports = {8080, 443};
+    // vector<string> server_names = {"zechi.com", "www.nigro.com"};
+    // long long client_max_body_size = 10485760; // 10 MB
+    // vector<Location> locations = {loc1, loc2, loc3};
+    // string global_root = "/globalroot";
+    // pair<string, string> redirection = {"301", "www.ingtagram.com"};
+    // vector<string> indexes2 = {"indexserver.html", "homeserver.html"};
+    // bool autoindex2 = true;
+    // vector<string> error_pages = {"404.html", "500.html", "403.html"};
     
-    Server server(
-        ports,
-        server_names,
-        client_max_body_size,
-        locations,
-        global_root,
-        redirection,
-        indexes2,
-        autoindex2,
-        error_pages
-    );
+    // Server server(
+    //     ports,
+    //     server_names,
+    //     client_max_body_size,
+    //     locations,
+    //     global_root,
+    //     redirection,
+    //     indexes2,
+    //     autoindex2,
+    //     error_pages
+    // );
 
-    server.print_server_info();
+    // server.print_server_info();
+	const char *http_request = 
+	"GET / HTTP/1.1\r\n"
+	"Host: localhost\r\n"
+	"Content-Length: 13\r\n"
+	"tkharbir9a: sfdhaf\r\n"
+	"helloman: sdjanfd\r\n"
+	"\r\n"
+	"Hello, World!";
+	HttpRequest test(http_request);
+	if (test.get_status_code().empty())
+		test.display_request();
+	else
+		cout << RED << "error: " << test.get_status_code() << "\n" << RESET;
 }
