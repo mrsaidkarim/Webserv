e9862c438813958a71e0d06056a312d56b317c26
Said Karim <sirsaidkarim@gmail.com>
2024-12-10 20:56:27 +0100

    Add tests for WebServ class

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zelabbas <zelabbas@student.42.fr>          +#+  +:+       +#+        */
+/*   By: skarim <skarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 12:06:33 by skarim            #+#    #+#             */
-/*   Updated: 2024/12/01 15:32:22 by zelabbas         ###   ########.fr       */
+/*   Updated: 2024/12/10 20:48:47 by skarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include "Configuration/Location.hpp"
 #include "Configuration/Server.hpp"
 #include "Request/HttpRequest.hpp"
+#include "WebServ.hpp"
 
 int main(int ac, char **av)
 {
@@ -74,18 +75,77 @@ int main(int ac, char **av)
     //     error_pages
     // );
 
+    // test run servers
+    Location loc1(
+        {"home", "repo1", "index.html"},
+        {"home.html"},
+        false,
+        "",
+        {{"GET", true},
+        {"POST", false},
+        {"DELETE", false}},
+        {}
+    );
+    Location loc2(
+        {"home", "repo2", "index.html"},
+        {},
+        false,
+        "",
+        {{"GET", true},
+        {"POST", false},
+        {"DELETE", false}},
+        {"301", "www.youtube.com"}
+    );
+    Location loc3(
+        {"home", "repo3", "index.html"},
+        {},
+        false,
+        "",
+        {{"GET", true},
+        {"POST", false},
+        {"DELETE", false}},
+        {}
+    );
+    Server server1(
+        {8080},
+        {"zechi.com", "www.nigro.com"},
+        10485760,
+        {loc1, loc2},
+        "/globalroot1",
+        {"301", "www.ingtagram.com"},
+        {"indexserver1.html", "homeserver1.html"},
+        true,
+        {"404.html", "500.html", "403.html"}
+    );
+
+    Server server2(
+        {7070},
+        {"saim.com"},
+        10485760,
+        {loc3},
+        "/globalroot2",
+        {"301", "www.maroc.ma"},
+        {"indexserver2.html", "homeserver2.html"},
+        true,
+        {"404.html", "500.html", "403.html"}
+    );
+    vector<Server> servers = {server1, server2};
+    WebServ ws(servers);
+    ws.run_servers();
+
+    //
     // server.print_server_info();
-	const char *http_request = 
-	"GET / HTTP/1.1\r\n"
-	"Host: localhost\r\n"
-	"Content-Length: 13\r\n"
-	"transfer: code\r\n"
-	"test: HTTPHELLO\r\n"
-	"\r\n"
-	"Hello, World!";
-	HttpRequest test(http_request);
-	if (test.get_status_code().empty())
-		test.display_request();
-	else
-		cout << RED << "error: " << test.get_status_code() << "\n" << RESET;
+	// const char *http_request = 
+	// "GET / HTTP/1.1\r\n"
+	// "Host: localhost\r\n"
+	// "Content-Length: 13\r\n"
+	// "transfer: code\r\n"
+	// "test: HTTPHELLO\r\n"
+	// "\r\n"
+	// "Hello, World!";
+	// HttpRequest test(http_request);
+	// if (test.get_status_code().empty())
+	// 	test.display_request();
+	// else
+	// 	cout << RED << "error: " << test.get_status_code() << "\n" << RESET;
 }
