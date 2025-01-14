acc799c10de7662fbb02e3a53ef567c8cf661c9c
mrsaidkarim <sirsaidkarim@gmail.com>
2025-01-14 19:55:26 +0100

    Modify post method

--- Response/post_method.cpp
+++ Response/post_method.cpp
@@ -6,7 +6,7 @@
 /*   By: skarim <skarim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 12:54:20 by skarim            #+#    #+#             */
-/*   Updated: 2025/01/12 12:35:23 by skarim           ###   ########.fr       */
+/*   Updated: 2025/01/14 19:20:02 by skarim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -123,22 +123,111 @@ static string addPrefixBeforeCRLF(const string &input) {
     return result;
 }
 
-void HttpResponse::post_method() const {
+static string generate_file_name(const string &extension) {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    
+    // Extract seconds and microseconds
+    time_t rawTime = tv.tv_sec;
+    int microseconds = tv.tv_usec;
+    
+    // Convert to local time
+    struct tm *timeInfo = localtime(&rawTime);
+
+    // Create the formatted string
+    ostringstream oss;
+    oss << (timeInfo->tm_year + 1900) << "_"       // Full Year (e.g., 2025)
+        << (timeInfo->tm_mon + 1) << "_"           // Month
+        << timeInfo->tm_mday << "_"               // Day
+        << timeInfo->tm_hour << "_"               // Hours
+        << timeInfo->tm_min << "_"                // Minutes
+        << timeInfo->tm_sec << "_"                // Seconds
+        << microseconds << "." << extension;               // Microseconds
+
+    return oss.str();
+}
+
+// fstream *HttpResponse::binary_post_case()
+// {
+//     // fstream *file = request->get_file_stream();
+//     // if (body.empty())
+//     // {
+//     //     request->set_is_complete(true); 
+//     //     return ;
+//     // }
+//     // cout << "bdat hna\n";
+//     // cout << BG_YELLOW << "===================== current body ============================\n";
+//     // cout << addPrefixBeforeCRLF(body) << "\n"; // to remove
+//     // cout << "===============================================================\n" << RESET;
+//     // if (!request->get_file_stream())
+//     // {
+    
+//     map<string, string>	header = request->get_header();
+//     string content_type = header["content-type"];
+//     string generated_binary_file = generate_file_name(content_type.substr(content_type.find("/") + 1));
+//     cout << BOLD_MAGENTA << "halfile >>>>>>>>>>>> " << generated_binary_file << endl << RESET;
+//     fstream *file = new fstream((POST_PATH + generated_binary_file).c_str(), ios::out | ios::trunc | ios::binary);
+//     if (!file->is_open()) {
+//         cerr << "Couldn't create the new file\n";
+//         perror("why?");
+//         request->set_is_complete(true);
+//         cout << BOLD_GREEN << "we read all 2\n\n";
+//         delete file;
+//         file = NULL;
+//         return NULL;
+//     }
+//     return file;
+//     //     request->set_file_stream(file);
+//     //     body = body.substr(2); // erase the first crlf
+//     // }
+//     // if (body.find(CRLF) != string::npos)
+//     // {
+//     //     request->set_is_complete(true);
+//     //     body = body.substr(0, body.find(CRLF)) + body.substr(body.find(CRLF) + 2); // erase the last crlf
+//     // }
+//     // *request->get_file_stream() << addPrefixBeforeCRLF(body);
+//     // request->set_body("");
+//     // cout << "selat hna hhhh\n";
+// }
+
+
+fstream *HttpResponse::binary_post_case()
+{
+    map<string, string>	header = request->get_header();
+    string content_type = header["content-type"];
+    string generated_binary_file = generate_file_name(content_type.substr(content_type.find("/") + 1));
+    cout << BOLD_MAGENTA << "halfile >>>>>>>>>>>> " << generated_binary_file << endl << RESET;
+    fstream *file = new fstream((POST_PATH + generated_binary_file).c_str(), ios::out | ios::trunc | ios::binary);
+    if (!file->is_open()) {
+        cerr << "Couldn't create the new file\n";
+        perror("why?");
+        request->set_is_complete(true);
+        cout << BOLD_GREEN << "we read all 2\n\n";
+        delete file;
+        file = NULL;
+        return NULL;
+    }
+    return file;
+}
+
+void HttpResponse::post_method() {
     string body = request->get_body();
+    if (body.empty())
+        request->set_is_complete(true);
     // cout << BOLD_RED << "we are in post method function" << "\n"; // to remove
-    auto header = request->get_header();
-    cout << BOLD_RED;
-    for(auto element = header.begin(); element != header.end(); element++)
-        cout << element->first << ": " << element->second << "\n";
-    cout << RESET;
+    // auto header = request->get_header();
+    // cout << BOLD_RED;
+    // for(auto element = header.begin(); element != header.end(); element++)
+    //     cout << element->first << ": " << element->second << "\n";
+    // cout << RESET;
     // cout << BG_YELLOW << "===================== current body before ============================\n";
     // cout << addPrefixBeforeCRLF(body) << "\n"; // to remove
     // cout << "===============================================================\n" << RESET;
     if (request->get_is_chunked())
         normalize_chunked_data(body);
-    cout << BG_BLUE << "===================== current body after ============================\n";
-    cout << addPrefixBeforeCRLF(body) << "\n"; // to remove
-    cout << "===============================================================\n" << RESET;
+    // cout << BG_BLUE << "===================== current body after ============================\n";
+    // cout << addPrefixBeforeCRLF(body) << "\n"; // to remove
+    // cout << "===============================================================\n" << RESET;
     fstream *file = request->get_file_stream(); // get file from request
     string slice;
     size_t pos_crlf = body.find(CRLF);
@@ -152,7 +241,7 @@ void HttpResponse::post_method() const {
     //     cout << BG_GREEN << "completed here\n" << RESET;
     //     // cout << body << "\n" << RESET;
     // }
-    if (pos_crlf == string::npos) {
+    if (pos_crlf == string::npos && !request->get_is_binary_post()) {
         if (!file)
             cerr << BOLD_RED << "can't write in file in post_method() func 1\n" << RESET;
         else {
@@ -163,9 +252,9 @@ void HttpResponse::post_method() const {
         // cout << BG_BLUE << "reh treseta lbody\n" << RESET;
         return ;
     }
-    cout << BG_BLUE << "ymken makynash " << RESET;
-    while (pos_crlf != string::npos) {
-        cout << BG_CYAN << "keyn am3alem " << RESET;
+    // cout << BG_BLUE << "ymken makynash crlf" << RESET;
+    while (pos_crlf != string::npos || request->get_is_binary_post()) {
+        // cout << BG_CYAN << "keyn am3alem crlf" << RESET;
         pos_bound_begin = body.find(request->get_boundary_key_begin());
         if (!request->get_boundary_key_begin().empty() && pos_bound_begin != string::npos) {
             cout << BG_GREEN << "keyn boundary_key_begin" << RESET;
@@ -221,11 +310,11 @@ void HttpResponse::post_method() const {
             request->set_body(body);
         }
         else {
-            cout << BG_YELLOW << "keyn boundary_key_end" << RESET;
+            // cout << BG_YELLOW << "i9der ikon boundary_key_end" << RESET;
             pos_bound_end = body.find(request->get_boundary_key_end());
             // we are done
             if (!request->get_boundary_key_end().empty() && pos_bound_end != string::npos) {
-                // cout << "---------------> found boundary key end \n";
+                cout << "---------------> found boundary key end \n";
                 slice = body.substr(0, pos_bound_end);
                 if (!file) {
                     cerr << BOLD_RED << "can't write in file in post_method() func 2\n" << RESET;
@@ -233,7 +322,7 @@ void HttpResponse::post_method() const {
                     cout << BOLD_GREEN << "we read all 3\n\n";
                 }
                 else {
-                    *file << addPrefixBeforeCRLF(slice);
+                    *file << slice;
                     cout << "here\n";
                 }
 
@@ -248,13 +337,6 @@ void HttpResponse::post_method() const {
             // }
             }
             else {
-                cout << BG_GREEN << "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh\n" << RESET;
-                if (request->get_is_binary_post())
-                {
-                    
-                    cout << BG_RED << "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhh" << endl << RESET;
-                    exit(1);
-                }
                 size_t last = body.rfind(CRLF);
                 // if (body.rfind("\r") != string::npos && last != string::npos)
                 //     last = (last > body.rfind("\r")) ? last : body.rfind("\r");
@@ -262,20 +344,40 @@ void HttpResponse::post_method() const {
                 //     *file << addPrefixBeforeCRLF(body); // Write remaining body
                 //     request->set_body("");
                 // }
+                if (request->get_is_binary_post())
+                {
+                    if (!file)
+                    {
+                        file = binary_post_case();
+                        body = body.substr(2);
+                    }
+                    if (!file)
+                        return ;
+                    request->set_file_stream(file);
+                }
                 if (last != string::npos && body.length() - last < 500)
                 {
                     // cout << BOLD_GREEN << "lbody li t7at: " <<  addPrefixBeforeCRLF(body.substr(0, last)) << endl << RESET;
                     *file << body.substr(0, last);
                     request->set_body(body.substr(last));
+                    if (request->get_is_binary_post())
+                        request->set_content_length(request->get_content_length() - last);
+                        // request->content_length -= last;
                     // cout << BOLD_RED << "*************: " << endl << RESET;
                     // << addPrefixBeforeCRLF(body.substr(last)) << endl << RESET;
                 }
                 else
                 {
-                    *file << body;
-                    request->set_body("");
-                    // cout << BG_RED << 
+                    if (request->get_is_binary_post())
+                        request->set_content_length(request->get_content_length() - body.size());
+                    // else
+                    // {
+                        *file << body;
+                        request->set_body("");
+                    // }
                 }
+                if (request->get_is_binary_post() && request->get_content_length() <= 0)
+                    request->set_is_complete(true);
                 // cout << BOLD_CYAN << "hedshi thal flfile: " << addPrefixBeforeCRLF(body.substr(0, pos_crlf)) << endl << RESET;
                 // body = body.substr(pos_crlf + 2);
                 
@@ -286,47 +388,21 @@ void HttpResponse::post_method() const {
         }
         pos_crlf = body.find(CRLF);
     }
-    cout << BG_BLUE << "CRLF" << RESET;
-
+    // cout << BG_BLUE << "ghadi ikhroj men post\n" << RESET;
 }
 
-
-static string generate_file_name(const string &extension) {
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    
-    // Extract seconds and microseconds
-    time_t rawTime = tv.tv_sec;
-    int microseconds = tv.tv_usec;
-    
-    // Convert to local time
-    struct tm *timeInfo = localtime(&rawTime);
-
-    // Create the formatted string
-    ostringstream oss;
-    oss << (timeInfo->tm_year + 1900) << "_"       // Full Year (e.g., 2025)
-        << (timeInfo->tm_mon + 1) << "_"           // Month
-        << timeInfo->tm_mday << "_"               // Day
-        << timeInfo->tm_hour << "_"               // Hours
-        << timeInfo->tm_min << "_"                // Minutes
-        << timeInfo->tm_sec << "_"                // Seconds
-        << microseconds << "." << extension;               // Microseconds
-
-    return oss.str();
-}
-
-void HttpResponse::binary_post_case()
-{
-    string content_type = request->get_header().find("content-type");
-    string generated_binary_file = generate_file_name(content_type.substr(content_type.find("/")));
-    fstream *file = new fstream((generated_binary_file).c_str(), ios::out | ios::trunc | ios::binary);
-    if (!file->is_open()) {
-        cerr << "Couldn't create the new file\n";
-        perror("why?");
-        request->set_is_complete(true);
-        cout << BOLD_GREEN << "we read all 2\n\n";
-        delete file;
-        file = NULL;
-        return;
-    }
-}
+// void HttpResponse::binary_post_case()
+// {
+//     string content_type = request->get_header().find("content-type");
+//     string generated_binary_file = generate_file_name(content_type.substr(content_type.find("/")));
+//     fstream *file = new fstream((generated_binary_file).c_str(), ios::out | ios::trunc | ios::binary);
+//     if (!file->is_open()) {
+//         cerr << "Couldn't create the new file\n";
+//         perror("why?");
+//         request->set_is_complete(true);
+//         cout << BOLD_GREEN << "we read all 2\n\n";
+//         delete file;
+//         file = NULL;
+//         return;
+//     }
+// }
